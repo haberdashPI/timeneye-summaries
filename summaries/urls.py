addffe63c0ed46f8a0ee8321ee7d9151b0d90350
David Little <david.frank.little@gmail.com>
2019-05-04 11:39:43 -0400

    Working server, setting up client now.

--- summaries/urls.py
+++ summaries/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('authenticate', views.authenticate)
+    path('authenticate', views.authenticate),
+    path('summary/<str:token>', views.summary)
 ]
